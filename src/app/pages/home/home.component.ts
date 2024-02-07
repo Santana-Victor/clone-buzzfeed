83d406a361ada9da8d05d7cc6bc14c9a73497a17
Victor Santana <santanavictor592@gmail.com>
2024-02-07 09:48:07 -0300

    feat: add page Home

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+import { QuizComponent } from '../../components/quiz/quiz.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [QuizComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+})
+export class HomeComponent {}
